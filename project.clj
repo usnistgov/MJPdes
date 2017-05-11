1b10e05aa353fcd391ec16388b9ebacef45e0e63
Peter Denno <podenno@gmail.com>
2017-05-11 11:29:57 -0400

    minor update

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject gov.nist/MJPdes "0.2.0"
   :description "A discrete event simulation engine for Multi-Job Production"
-  :url "https://github.com/usnistgov/desim"
+  :url "https://github.com/usnistgov/MJPdes"
   :license {:name "See the github site for details"
             :url "https://github.com/usnistgov/MJPdes"}
   :profiles {:uberjar {:aot :all}}
   :plugins [[lein-bin "0.3.4"]
             [lein-cljfmt "0.5.3"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [medley "0.8.3"]
+                 [medley "0.8.4"]
                  [incanter/incanter-core "1.9.1"]]
   :bin {:name "MJPdes"
         :bootclasspath true}
